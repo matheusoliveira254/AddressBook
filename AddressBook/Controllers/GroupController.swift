4a6a06a6ac6216ce6968e7c966a2dc5594367ced
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-09-26 17:17:58 -0600

    creation of the save and load functions for the persistence of data

--- AddressBook/Controllers/GroupController.swift
+++ AddressBook/Controllers/GroupController.swift
@@ -41,10 +41,35 @@ class GroupController {
     //MARK: - Persistence
     
     func saveContactsToDisk() {
-        
+        //get the address and make sure is not nil
+        guard let url = fileURL else {return}
+        //convert to swift struct or class into raw data
+        do {
+            let data = try JSONEncoder().encode(groups)
+            // save the data to the address from url
+            try data.write(to: url)
+        } catch let error {
+            print(error)
+        }
     }
     
     func loadContactsFromDisk() {
-        
+        //make sure file location !=nil and going to the location
+        guard let loadLocation = fileURL else {return}
+            
+        do {
+            let data = try Data(contentsOf: loadLocation)
+            let decodeData = try JSONDecoder().decode([Group].self, from: data)
+            self.groups = decodeData
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    //Computed Property
+    private var fileURL: URL? {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return nil}
+        let finalUrl = documentsDirectory.appendingPathComponent("groups.json")
+        return finalUrl
     }
 } // End of Class
