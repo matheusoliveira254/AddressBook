005af254fb37873c271c02d0a9100cf52f897ca3
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-09-28 17:19:08 -0600

    added the custom cell file and the new elements for the implementation of the custom cell and favorite buttons

---
+++ AddressBook/Controllers/View Controllers/Custom View/PersonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonTableViewCell.swift
+//  AddressBook
+//
+//  Created by Matheus Oliveira on 9/28/22.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
